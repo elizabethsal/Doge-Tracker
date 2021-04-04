c5370e2f7b7102f0540e5af41367f0b7b5b73a3b
DariaTarasova <dasharil1997@gmail.com>
2021-04-04 23:28:28 +0300

    Created SystemLocationManager, created exceptions, if we don't have permissions. Passed Lifecicle methods to MapView.

--- app/src/main/java/com/example/dogetracker/fragments/MapFragment.java
+++ app/src/main/java/com/example/dogetracker/fragments/MapFragment.java
@@ -19,7 +19,7 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MapFragment extends Fragment implements OnMapReadyCallback {
 
-    public MapView mapView;
+    private MapView mapView;
     private GoogleMap googleMap;
 
 
@@ -27,8 +27,12 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_map, container, false);
-
     }
+
+    /**
+     * Check location permissions
+     * if "yes" subscribe to location changes via SystemLocationManager
+     * checking locationPermissions before every attempt of subscribing to location permissions*/
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
@@ -51,43 +55,53 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
         super.onCreate(savedInstanceState);
     }
 */
-    @Override
-    public void onStart() {
-        super.onStart();
-    }
-/*
-    @Override
-    public void onResume() {
-        super.onResume();
-    }
-*/
-    @Override
-    public void onPause() {
-        super.onPause();
-    }
 
-    @Override
-    public void onStop() {
-        super.onStop();
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-    }
-
-    @Override
-    public void onSaveInstanceState(@NonNull Bundle outState) {
-        super.onSaveInstanceState(outState);
-    }
-
-    @Override
-    public void onLowMemory() {
-        super.onLowMemory();
-    }
 
     @Override
     public void onMapReady(GoogleMap map) {
         googleMap = map;
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
 }
--- app/src/main/java/com/example/dogetracker/location/SystemLocationManager.java
+++ app/src/main/java/com/example/dogetracker/location/SystemLocationManager.java
@@ -1,4 +1,65 @@
 package com.example.dogetracker.location;
 
-public class SystemLocationManager {
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.location.LocationProvider;
+import android.os.Bundle;
+
+import androidx.core.app.ActivityCompat;
+
+
+import java.util.List;
+
+import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+
+public class SystemLocationManager implements LocationListener {
+
+    private LocationManager locationManager;
+    private static final long MIN_UPDATE_INTERVAL_MS = 1000;
+    private static final float MIN_DISTANCE_CHANGE_UPDATES_M = 0;
+
+
+    public SystemLocationManager(Context context) {
+        if ((ActivityCompat.checkSelfPermission(context, ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) || (ActivityCompat.checkSelfPermission(context, ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)) {
+            throw new SecurityException("Didn't got location permissions");
+        }
+
+        locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+        if(locationManager==null){
+            throw new NullPointerException("Location manager is null");
+        }
+
+        List<String> providers = locationManager.getAllProviders();
+        if (!providers.contains(LocationManager.GPS_PROVIDER) || !providers.contains(LocationManager.NETWORK_PROVIDER)){
+            throw new NullPointerException("No location providers");
+        }
+
+        locationManager.requestLocationUpdates(providers.get(0), MIN_UPDATE_INTERVAL_MS, MIN_DISTANCE_CHANGE_UPDATES_M, this);
+
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
 }
